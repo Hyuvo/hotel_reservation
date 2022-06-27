fa57452130ba2baf8f45a54c26d22871c1072f8c
xiecongyang <xiecongyang@outlook.com>
2022-06-27 19:26:34 -0400

    Created api package and added HotelResource class to it

--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -2,12 +2,11 @@ package service;
 
 import model.Customer;
 
-import java.util.Collection;
 import java.util.HashMap;
 
 public class CustomerService {
     private static HashMap<String, Customer> customers = new HashMap<>();
-    public void addCusTomer(String email, String firstName, String lastName) {
+    public void addCustomer(String email, String firstName, String lastName) {
         customers.put(email, new Customer(firstName, lastName, email));
     }
     public Customer getCustomer(String customerEmail) {
